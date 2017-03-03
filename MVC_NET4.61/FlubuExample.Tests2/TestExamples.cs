5b316a8515ab5059e2c7b33803bc18003dbba20a
markoz <mzorec@gmail.com>
2017-03-03 20:42:18 +0100

    missing file

---
+++ MVC_NET4.61/FlubuExample.Tests2/TestExamples.cs
@@ -0,0 +1,59 @@
+﻿using System.Xml;
+using NUnit.Framework;
+
+namespace FlubuExample.Tests2
+{
+    public class TestExamples
+    {
+        [Test]
+        public void Test1()
+        {
+            var flName = typeof(XmlDocument).Assembly.FullName;
+            Assert.AreEqual(1, 1);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var flName = typeof(XmlDocument).Assembly.FullName;
+            Assert.AreEqual(1, 1);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var flName = typeof(XmlDocument).Assembly.FullName;
+            Assert.AreEqual(1, 1);
+        }
+        [Test]
+        public void Test4()
+        {
+            var flName = typeof(XmlDocument).Assembly.FullName;
+            Assert.AreEqual(1, 1);
+        }
+        [Test]
+        public void Test5()
+        {
+            var flName = typeof(XmlDocument).Assembly.FullName;
+            Assert.AreEqual(1, 1);
+        }
+        [Test]
+        public void Test6()
+        {
+            var flName = typeof(XmlDocument).Assembly.FullName;
+            Assert.AreEqual(1, 1);
+        }
+        [Test]
+        public void Test7()
+        {
+            var flName = typeof(XmlDocument).Assembly.FullName;
+            Assert.AreEqual(1, 1);
+        }
+        [Test]
+        public void Test8()
+        {
+            var flName = typeof(XmlDocument).Assembly.FullName;
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
