624ce39e3489e08b1d5639673aeb2a828b5549c4
markoz <mzorec@gmail.com>
2017-09-25 19:48:58 +0200

    missing files

---
+++ DeployScriptExample/BuildScript/BuildScript.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FlubuCore.Context;
+using FlubuCore.Packaging;
+using FlubuCore.Scripting;
+
+namespace BuildScript.BuildScript
+{
+    public class BuildScript : DefaultBuildScript
+    {
+        protected override void ConfigureBuildProperties(IBuildPropertiesContext context)
+        {
+            context.Properties.Set(BuildProps.FlubuWebApiBaseUrl, "http://localhost:5000/");
+        }
+
+        /// <summary>
+        /// Script takes already prepared deploy package. Normaly your build server would prepare the package.
+        /// </summary>
+        /// <param name="session"></param>
+        protected override void ConfigureTargets(ITaskContext session)
+        {
+            session.CreateTarget("package")
+                .AddTask(x => x.PackageTask("").AddDirectoryToPackage("WebApplication1\\Scripts", "ExampleApp\\Scripts", true)
+                    .AddDirectoryToPackage("WebApplication1\\Views", "ExampleApp\\Views", true)
+                    .AddDirectoryToPackage("WebApplication1\\Content", "ExampleApp\\Content", true)
+                    .AddDirectoryToPackage("WebApplication1\\Bin", "ExampleApp\\Bin", true)
+                    .AddDirectoryToPackage("WebApplication1", "ExampleApp", false, new NegativeFilter(new RegexFileFilter(@"^.*\.(svc|asax|config|js|html|ico|bat)$")))
+                    .ZipPackage("ExampleApp.zip"));
+
+            session.CreateTarget("deploy")
+                .SetAsDefault()
+                .AddTask(x => x.FlubuWebApiTasks().GetTokenTask("test", "test"))
+                .AddTask(x => x.FlubuWebApiTasks().DeletePackagesTask())
+                .AddTask(x => x.FlubuWebApiTasks().UploadPackageTask(".\\BuildScript\\ExamplePackages", "*.zip"))
+                .AddTask(x => x.FlubuWebApiTasks().ExecuteScriptTask("deploy", "DeployScript.cs"));
+        }
+    }
+}
---
+++ DeployScriptExample/BuildScript/DeployScript.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using FlubuCore.Context;
+using FlubuCore.Scripting;
+using FlubuCore.Tasks.Iis;
+
+namespace BuildScript
+{
+    public class DeployScript : DefaultBuildScript
+    {
+        protected override void ConfigureBuildProperties(IBuildPropertiesContext context)
+        {
+        }
+
+        protected override void ConfigureTargets(ITaskContext session)
+        {
+            session.CreateTarget("deploy")
+                .AddTask(x =>
+                    x.IisTasks().CreateAppPoolTask("Example app pool")
+                        .Mode(CreateApplicationPoolMode.DoNothingIfExists))
+                .AddTask(x =>
+                    x.IisTasks().ControlAppPoolTask("Example app pool", ControlApplicationPoolAction.Stop)
+                        .DoNotFailOnError())
+                .Do(UnzipPackage)
+                .AddTask(x => x.CopyDirectoryStructureTask(@"Packages\ExampleApp", @"C:\ExampleApp", true).Retry(20, 5000))
+                .Do(CreateWebSite)
+                .AddTask(x => x.IisTasks().ControlAppPoolTask("Example app pool", ControlApplicationPoolAction.Start));
+
+
+            session.CreateTarget("deployTest3");
+
+        }
+
+        public void UnzipPackage(ITaskContext context)
+        {
+            var files = Directory.EnumerateFiles("packages", "*.zip").ToList();
+            string zip = files[0];
+            context.Tasks().UnzipTask(zip, "packages").Execute(context);
+        }
+
+        public void CreateWebSite(ITaskContext context)
+        {
+            context.Tasks().IisTasks().CreateWebsiteTask()
+                .ApplicationPoolName("Example app pool")
+                .WebsiteName("Example web site")
+                .BindingProtocol("Http")
+                .Port(3080)
+                .PhysicalPath("C:\\ExampleApp")
+                .WebsiteMode(CreateWebApplicationMode.DoNothingIfExists).Execute(context);
+        }
+    }
+}
