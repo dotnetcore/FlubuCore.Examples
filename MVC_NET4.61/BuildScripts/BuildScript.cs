ae5b52a5c530ba9021633e651503a5a580d0ca83
markoz <mzorec@gmail.com>
2017-12-16 10:38:23 +0100

    fixed cast

--- MVC_NET4.61/BuildScripts/BuildScript.cs
+++ MVC_NET4.61/BuildScripts/BuildScript.cs
@@ -72,7 +72,7 @@ public class BuildScript : DefaultBuildScript
 
         ////Run build.exe Rebuild.Server -exampleArg=someValue to pass argument
         var doAsyncExample = session.CreateTarget("DoAsync.Example")
-           .DoAsync((Action<ITaskContextInternal>)DoAsyncExample, session.ScriptArgs["exampleArg"])
+           .DoAsync((Action<ITaskContextInternal, string>)DoAsyncExample, session.ScriptArgs["exampleArg"])
            .DoAsync((Action<ITaskContextInternal>)DoAsyncExample2);
   
         session.CreateTarget("iis.install").Do(IisInstall);
