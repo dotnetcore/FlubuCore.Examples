edb2974d037a3ede6a03c60d4067f39f1e7356c2
markoz <mzorec@gmail.com>
2018-01-11 17:26:40 +0100

    missing files

---
+++ NetCore_csproj/BuildScript/BuildScriptHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class BuildScriptHelper
+{
+    public static void SomeMethod()
+    {
+        Console.WriteLine("Some method");
+    }
+}
+
---
+++ NetCore_csproj/FlubuExample.Tests/TargetTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Xml;
+using Xunit;
+using NUnit;
+namespace Flubu.Tests
+{
+    public class TargetTests
+    {
+             [Fact]
+        public void Test()
+        {
+            var flName = typeof(XmlDocument).AssemblyQualifiedName;
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal(1, 1);
+            var flName = typeof(NUnit.Framework.Assert).AssemblyQualifiedName;
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            Assert.Equal(1, 1);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            Assert.Equal(1, 1);
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            Assert.Equal(1, 1);
+        }
+
+        [Fact]
+        public void Test6()
+        {
+            Assert.Equal(1, 1);
+        }
+
+        [Fact]
+        public void Test7()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
