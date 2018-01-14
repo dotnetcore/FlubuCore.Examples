5543759ef8d50d6fc75a96f569359d6c22bc03d5
markoz <mzorec@gmail.com>
2018-01-14 15:50:01 +0100

    Flubu custom task plugin and load assemblies from directory example.

---
+++ FlubuCustomTaskPluginAndLoadAssembliesFromDirectoryExample/FlubuCore.PluginExample/ExampleFlubuPluginTask.cs
@@ -0,0 +1,33 @@
+﻿using FlubuCore.Context;
+using FlubuCore.Tasks;
+
+namespace FlubuCore.PluginExample
+{
+    public class ExampleFlubuPluginTask : TaskBase<int, ExampleFlubuPluginTask>
+    {
+        private string _description;
+
+        private string _message;
+
+        public ExampleFlubuPluginTask ExampleFluentInterface(string message)
+        {
+            _message = message;
+            return this;
+        }
+
+        protected override int DoExecute(ITaskContextInternal context)
+        {
+            //// write task logic here.
+            context.LogInfo(!string.IsNullOrEmpty(_message) ? _message : "Just some dummy code");
+
+            return 0;
+        }
+
+        protected override string Description
+        {
+            get => string.IsNullOrEmpty(_description) ? $"Example plugin task.." : _description;
+
+            set => _description = value;
+        }
+    }
+}
---
+++ FlubuCustomTaskPluginAndLoadAssembliesFromDirectoryExample/FlubuCore.PluginExample/TaskFluentInterfaceExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FlubuCore.Context.FluentInterface.Interfaces;
+using FlubuCore.PluginExample;
+
+// ReSharper disable once CheckNamespace
+namespace FlubuCore.Context.FluentInterface.Interfaces
+{
+   public static class TaskFluentInterfaceExtension
+    {
+        public static ExampleFlubuPluginTask ExampleFlubuPluginTask(this ITaskFluentInterface flubu)
+        {
+            return new ExampleFlubuPluginTask();
+        }
+    }
+}
