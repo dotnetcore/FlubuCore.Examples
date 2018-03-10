30092c5a67542cedfffd5f9b2cf212c50adc4bf6
markoz <mzorec@gmail.com>
2018-03-10 23:10:54 +0100

    How to test build script example

---
+++ NetCore_csproj/BuildScript/BuildScriptForTestsAndDebugExample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using FlubuCore.Context;
+using FlubuCore.Scripting;
+
+namespace BuildScript.BuildScript
+{
+    public class BuildScriptForTestsAndDebugExample : DefaultBuildScript
+    {
+        protected override void ConfigureBuildProperties(IBuildPropertiesContext context)
+        {
+        }
+
+        protected override void ConfigureTargets(ITaskContext session)
+        {
+            session.CreateTarget("TestAndDebugExample")
+                .SetAsDefault()
+                .Do(CreateFile);
+        }
+
+
+        public void CreateFile(ITaskContext context)
+        {
+            File.Create("test123.txt");
+        } 
+    }
+}
---
+++ NetCore_csproj/BuildScript/BuildScriptTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using FlubuCore.Scripting;
+using Xunit;
+
+namespace BuildScript.BuildScript
+{
+    /// <summary>
+    /// You can debug scripts with tests.
+    /// </summary>
+    public class BuildScriptTests
+    {
+        [Fact]
+        public void TestTargetExample()
+        {
+            if (File.Exists("test123.txt"))
+            {
+                File.Delete("test123.txt");
+            }
+
+            IFlubuEngine engine = new FlubuEngine();
+
+            var session = engine.CreateTaskSession(new BuildScriptArguments
+            {
+                MainCommands = new List<string>()
+                {
+                    "TestAndDebugExample"
+                }
+            });
+
+            BuildScriptForTestsAndDebugExample bs = new BuildScriptForTestsAndDebugExample();
+
+            var result = bs.Run(session);
+            Assert.Equal(0, result);
+            Assert.True(File.Exists("test123.txt"));
+        }
+
+        [Fact]
+        //// You can just test your code in do (Custom code)
+        public void TestDoExample()
+        {
+            if (File.Exists("test123.txt"))
+            {
+                File.Delete("test123.txt");
+            }
+
+            BuildScriptForTestsAndDebugExample bs = new BuildScriptForTestsAndDebugExample();
+            
+            IFlubuEngine engine = new FlubuEngine();
+
+            var session = engine.CreateTaskSession(new BuildScriptArguments());
+            bs.CreateFile(session);
+            Assert.True(File.Exists("test123.txt"));
+        }
+    }
+}
