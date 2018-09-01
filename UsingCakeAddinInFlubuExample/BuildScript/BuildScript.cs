f469e94b0251b2f56e916526a0cdea448bfddd43
markoz <mzorec@gmail.com>
2018-09-01 04:27:28 +0200

    Adds example of Cake addin usage in FlubuCore.

---
+++ UsingCakeAddinInFlubuExample/BuildScript/BuildScript.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Cake.Common.Build;
+using Cake.FileHelpers;
+using FlubuCore.Context;
+using FlubuCore.Scripting;
+
+//#nuget FlubuCore.CakePlugin, 1.0.0
+//#nuget Cake.Core, 0.30.0
+//#nuget Cake.FileHelpers, 3.1.0
+//#nuget Cake.Common, 0.30.0
+
+namespace UsingCakeAddinInFlubuExample
+{
+   /// <summary>
+   /// Example uses Cake.FileHelpers plugin.
+   /// This is just an example that using Cake addins works. Using this addin in real scenario would be meaningless as you could just use File.ReadAllLines() with FlubuCore.
+   /// </summary>
+    public class BuildScript : DefaultBuildScript
+    {
+        protected override void ConfigureBuildProperties(IBuildPropertiesContext context)
+        {
+        }
+
+        protected override void ConfigureTargets(ITaskContext context)
+        {
+            context.CreateTarget("Cake.Addion")
+                .SetAsDefault()
+                .Do(CakeAddinExample);
+        }
+        
+        private void CakeAddinExample(ITaskContext context)
+        {
+            var test = context.CakeTasks().Bitrise().IsRunningOnBitrise;
+            context.LogInfo($"IsRunning on bitrise: {test}");
+            var lines = context.CakeTasks().FileReadLines("ExampleFile.txt");
+            context.LogInfo(lines[0]);
+        }
+    }
+}
