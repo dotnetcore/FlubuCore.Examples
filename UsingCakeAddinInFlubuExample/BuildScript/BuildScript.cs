8f9b6e2778298e99193227f1b57e12acdf30c755
markoz <mzorec@gmail.com>
2019-05-06 17:24:20 +0200

    updates cake addin example

--- UsingCakeAddinInFlubuExample/BuildScript/BuildScript.cs
+++ UsingCakeAddinInFlubuExample/BuildScript/BuildScript.cs
@@ -4,20 +4,16 @@ using Cake.FileHelpers;
 using FlubuCore.Context;
 using FlubuCore.Context.FluentInterface.Interfaces;
 using FlubuCore.Scripting;
-
-//#nuget FlubuCore.CakePlugin, 1.0.0
-//#nuget Cake.Core, 0.30.0
-//#nuget Cake.FileHelpers, 3.1.0
-//#nuget Cake.Common, 0.30.0
-//#nuget FlubuCore.Diff, 1.0.1
-
+using FlubuCore.Scripting.Attributes;
 
 namespace UsingCakeAddinInFlubuExample
 {
    /// <summary>
    /// Example uses Cake.FileHelpers addin.
    /// This is just an example that using Cake addins works. Using this addin in real scenario would be meaningless as you could just use File.ReadAllLines() with FlubuCore.
-   /// </summary>
+   /// </summary
+    [NugetPackage("Cake.FileHelpers", "3.1.0")]
+    [NugetPackage("FlubuCore.CakePlugin", "1.1.0")]
     public class BuildScript : DefaultBuildScript
     {
         protected override void ConfigureBuildProperties(IBuildPropertiesContext context)
@@ -26,14 +22,13 @@ namespace UsingCakeAddinInFlubuExample
 
         protected override void ConfigureTargets(ITaskContext context)
         {
-            context.CreateTarget("Cake.Addion")
+            context.CreateTarget("Cake.Addon")
                 .SetAsDefault()
                 .Do(CakeAddinExample);
         }
         
         private void CakeAddinExample(ITaskContext context)
         {
-            context.Tasks().DiffTask("1.txt", "2.txt", "output.html").Execute(context);
             var test = context.CakeTasks().Bitrise().IsRunningOnBitrise;
             context.LogInfo($"IsRunning on bitrise: {test}");
             var lines = context.CakeTasks().FileReadLines("ExampleFile.txt");
